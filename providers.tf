89e854130a0f9d9c92cd437b0d782deeeb4ead26
mayankGit-aws <mayankagrawalit@gmail.com>
2021-04-11 12:32:08 +0530

    removing S3 bucket

--- providers.tf
+++ providers.tf
@@ -2,11 +2,4 @@ provider "aws" {
   region = "${var.region}"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "javahome-tf-12345678"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "javahome-tf"
-  }
-}
+
